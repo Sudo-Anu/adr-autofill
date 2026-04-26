e7b83dcae791beaa299558ddfcfcef6927277acb
Sudo-Anu <anuragraut551@gmail.com>
2026-04-27 00:32:27 +0530

    final fix

--- app.py
+++ app.py
@@ -721,8 +721,5 @@ def build_adr_pdf(d):
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
-    app.run(debug=True, port=5000)
-
-if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
